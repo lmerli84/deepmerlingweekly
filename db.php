4ea92ffff1b4bc8c69033fdb8adecc64f97a72e5
lorenzo merli <l.merli84@gmail.com>
2019-12-31 10:31:11 +0100

    fix

--- db.php
+++ db.php
@@ -24,36 +24,10 @@ if (!$result)
 //echo mysqli_num_rows($result)
 
 //SELECT `ID`, `NAME`, `OWNER`, `DESCR`, `RULES`, `RATING` FROM `anag_game` WHERE 1
-
-
+include "header.php"
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>DeepMerlingWeekly.com</title>
-
-  <!-- CSS  -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-</head>
-<body>
-  <nav class="blue lighten-1" role="navigation">
-    <div class="nav-wrapper container">
-      <!-- <a id="logo-container" href="#" class="brand-logo">Logo che manca</a> -->
-      <ul class="right hide-on-med-and-down">
-          <li><a href="http://www.deepmerlingweekly.com">Home</a></li>
-          <li><a href="http://www.deepmerlingweekly.com/issues/issues.html">Issues</a></li>
-          <li><a href="http://www.deepmerlingweekly.com/subscribe.html" id="download-button" class="btn waves-effect waves-light yellow darken-2">Subscribe!</a></li>
-      </ul>
-    </div>
-  </nav>
-  <div class="section no-pad-bot" id="index-banner">
     <div class="container">
-      <br><br>
       <h2 class="header center yellow-text text-darken-2">Boardgames</h2>
 <div class="row center" id="issue_container">
 
---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>DeepMerlingWeekly.com</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="blue lighten-1" role="navigation">
+    <div class="nav-wrapper container">
+      <!-- <a id="logo-container" href="#" class="brand-logo">Logo che manca</a> -->
+      <ul class="right hide-on-med-and-down">
+          <li><a href="http://www.deepmerlingweekly.com">Home</a></li>
+          <li><a href="http://www.deepmerlingweekly.com/issues/issues.html">Issues</a></li>
+          <li><a href="http://www.deepmerlingweekly.com/subscribe.html" id="download-button" class="btn waves-effect waves-light yellow darken-2">Subscribe!</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
