dff6f0ff3067f04afe9e40b4e4af37827ee12a2e
lorenzo merli <l.merli84@gmail.com>
2019-12-31 10:13:47 +0100

    fixes

--- db.php
+++ db.php
@@ -35,15 +35,103 @@ $result = $connection->query($sql);
 if (!$result)
     die("Database access failed: " . mysqli_error());
 
-echo $sql;
-echo "<p> </p>";
-echo mysqli_num_rows($result);
+//echo $sql;
+//echo "<p> </p>";
+//echo mysqli_num_rows($result);
 
-echo "<b><center>Database Output</center></b><br><br>";
+
 //SELECT `ID`, `NAME`, `OWNER`, `DESCR`, `RULES`, `RATING` FROM `anag_game` WHERE 1
-while ($row = $result->fetch_assoc()) {  
 
-   echo "<u>".$row['ID']."<b>".$row['NAME']."</b></u>";
- }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>DeepMerlingWeekly.com</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="blue lighten-1" role="navigation">
+    <div class="nav-wrapper container">
+      <!-- <a id="logo-container" href="#" class="brand-logo">Logo che manca</a> -->
+      <ul class="right hide-on-med-and-down">
+          <li><a href="http://www.deepmerlingweekly.com">Home</a></li>
+          <li><a href="http://www.deepmerlingweekly.com/issues/issues.html">Issues</a></li>
+          <li><a href="http://www.deepmerlingweekly.com/subscribe.html" id="download-button" class="btn waves-effect waves-light yellow darken-2">Subscribe!</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br><br>
+      <h2 class="header center yellow-text text-darken-2">Boardgames</h2>
+<div class="row center" id="issue_container">
+
+<div class="row center">
+<?php
+while ($row = $result->fetch_assoc()) {  
+
+echo "<u>".$row['ID']."<b>".$row['NAME']."</b></u>";
+}
+?>
+</div>
+
+      </div>
+    </div>
+      <br><br>
+
+    </div>
+  </div>
+
+
+  <footer class="page-footer yellow darken-2">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">Nata per scherzo, in crescita costante, si spera non muoia</p>
+
+
+        </div>
+        <!-- <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>-->
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="yellow-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
+
+
+ <!--  Scripts-->
+ <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+ <script src="js/materialize.js"></script>
+
+
+  </body>
+</html>
