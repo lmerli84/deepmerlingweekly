ce0d444f7b2f43a5a3106182fb01226b6c214c2d
lorenzo merli <l.merli84@gmail.com>
2019-12-31 08:27:17 +0100

    php_part

---
+++ db.php
@@ -0,0 +1,45 @@
+<?php
+function db_connect() {
+
+        // Define connection as a static variable, to avoid connecting more than once 
+    static $connection;
+
+        // Try and connect to the database, if a connection has not been established yet
+    if(!isset($connection)) {
+             // Load configuration as an array. Use the actual location of your configuration file
+        $config = parse_ini_file('./secure/config.ini'); 
+        $connection = mysqli_connect($config['servername'],$config['username'],$config['password'],$config['dbname']);
+    }
+
+        // If connection was not successful, handle the error
+    if($connection === false) {
+            // Handle error - notify administrator, log to a file, show an error screen, etc.
+        return mysqli_connect_error(); 
+    }
+    return $connection;
+}
+
+// Connect to the database
+$connection = db_connect();
+
+// Check connection
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+ 
+$sql = "SELECT * FROM anag_game";
+$result = $connection->query($sql);
+
+echo $sql;
+
+$num=$result->num_rows;
+
+echo "<b><center>Database Output</center></b><br><br>";
+
+while ($row = $result->fetch_assoc()) {  
+
+   echo "<u>".$row['id']."</u><b>".$row['first']."</b>"; //etc...
+ }
+
+?>
